aa740539fae218a6e9694637c747e20894f6925b
Lorena Kim <iloveposhitos@gmail.com>
2023-09-06 20:53:50 -0500

    tengo hambre

---
+++ src/trabajofinalpoo/Corredor.java
@@ -0,0 +1,16 @@
+package trabajofinalpoo;
+
+public class Corredor {
+    private String tipo;
+
+    public Corredor(String tipo) {
+        setTipo(tipo);
+    }
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+    public String getTipo() {
+        return tipo;
+    }
+
+}
--- src/trabajofinalpoo/Main.java
+++ src/trabajofinalpoo/Main.java
@@ -1,20 +1,15 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package trabajofinalpoo;
-
-/**
- *
- * @author itzsm
- */
 public class Main {
-
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        System.out.println("QUIERO DORMIR");
+        System.out.println("QUIERO DORMIR, yo también");
+
+        Usuario usuario = new Universitario("Lorena", "Kim", "lorenakim@gmail.com",
+                "holahola"
+        );
+
+        usuario.setCorredor("amarillo");
+        System.out.println(usuario.getTarifaInicial());
     }
     
 }
